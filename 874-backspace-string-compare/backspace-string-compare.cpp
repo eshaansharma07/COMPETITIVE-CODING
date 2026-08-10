e80cd27e1c1737a38228df3bb2a2765f1926079f
Eshaan Sharma <eshaansharma800@gmail.com>
2026-08-10 14:35:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (30.30%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> a, b;
+
+        for (char c : s) {
+            if (c == '#') {
+                if (!a.empty())
+                    a.pop();
+            } else {
+                a.push(c);
+            }
+        }
+
+        for (char c : t) {
+            if (c == '#') {
+                if (!b.empty())
+                    b.pop();
+            } else {
+                b.push(c);
+            }
+        }
+
+        return a == b;
+    }
+};
